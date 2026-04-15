052fa0c6b34bc864ac636529447fb68e24a29c49
CotiMaria <emailul_tau@exemplu.com>
2026-04-15 16:13:38 +0300

    Eliminat test.py din repository (pastrat doar local)

--- test.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-
-print(f"{sys.executable}")
-if "anaconda" in sys.executable.lower() or "conda" in sys.executable.lower():
-    print("Succes")
-else:
-    print("insucces") 
