e44f67683b1ec9e4087dfd33e42b2b124f39ffeb
mihaela810 <dobrican.ma.ana@student.utcluj.ro>
2026-05-21 22:30:56 +0300

    fisiere de start/stop

---
+++ start.bat
@@ -0,0 +1,17 @@
+@echo off
+title M&M Bloom
+echo.
+echo  ==========================================
+echo   M&M Bloom - Pornire aplicatie
+echo  ==========================================
+echo.
+ 
+cd /d "C:\Users\T\OneDrive - Technical University of Cluj-Napoca\Desktop\Sistem_HR\Proiect11-.Sistem_HR_pentr_companie_IT"
+ 
+echo  [1/2] Pornire containere Docker...
+echo.
+docker-compose up
+ 
+echo.
+echo  Aplicatia s-a oprit.
+pause
---
+++ stop.bat
@@ -0,0 +1,4 @@
+@echo off
+cd /d "C:\Users\T\OneDrive - Technical University of Cluj-Napoca\Desktop\Sistem_HR\Proiect11-.Sistem_HR_pentr_companie_IT"
+docker-compose down
+pause
